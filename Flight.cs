4a7531766cc9b989bf3c98325ef36e932c7c57eb
Nikita Smirnov <cnism001@edu.xamk.fi>
2023-11-20 15:36:04 +0200

    Added Flight interface and implementations for it

---
+++ Flight.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_6_STRATEGY
+{
+    public interface IFlight
+    {
+        string Fly();
+    }
+
+    public class FlyWithWings : IFlight
+    {
+        public string Fly() => "FLIES BY FLAPPING ITS WINGS";
+    }
+
+    public class NoFly : IFlight
+    {
+        public string Fly() => "HIDES IT'S HEAD IN THE SAND AS IT CANNOT FLY";
+    }
+
+    public class Slide : IFlight
+    {
+        public string Fly() => "SLIDES ON IT'S BELLY";
+    }
+
+    public class Float : IFlight
+    {
+        public string Fly() => "FLOATS IN WATER";
+    }
+
+}
