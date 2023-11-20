4dc7f54b5f90f6abb359c7bc55087adb49e96ecf
Nikita Smirnov <cnism001@edu.xamk.fi>
2023-11-20 16:01:42 +0200

    added Bird class with its method and inheriting bird subclasses

---
+++ Bird.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_6_STRATEGY
+{
+    public abstract class Bird
+    {
+        //can only be accesses from Bird class or its inheritors
+        protected IFlight flight { get; set; }
+
+        // Method to perform the fly action, which utilizes the IFlight's Fly method
+        public string PerformFly() => flight.Fly();
+    }
+
+    // MallardDuck class inheriting from Bird, same for other bird subclasses
+    public class MallardDuck : Bird
+    {
+        public MallardDuck()
+        {
+            this.flight = new FlyWithWings();
+        }
+    }
+
+    public class EmperorPenguin : Bird
+    {
+        public EmperorPenguin()
+        {
+            this.flight = new Slide();
+        }
+    }
+
+    public class BaldEagle : Bird
+    {
+        public BaldEagle()
+        {
+            this.flight = new FlyWithWings();
+        }
+    }
+
+    public class Ostrich : Bird
+    {
+        public Ostrich()
+        {
+            this.flight = new NoFly();
+        }
+    }
+
+    public class YellowRubberDuck : Bird
+    {
+        public YellowRubberDuck()
+        {
+            this.flight = new Float();
+        }
+    }
+
+}
