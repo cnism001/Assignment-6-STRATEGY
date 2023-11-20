43cac1ac2990324e4f51a7a391acf169cacd47fb
Nikita Smirnov <cnism001@edu.xamk.fi>
2023-11-20 16:08:07 +0200

    added list of birds and loop to print out their type and flight pattern

--- Program.cs
+++ Program.cs
@@ -10,6 +10,22 @@ namespace Assignment_6_STRATEGY
     {
         static void Main(string[] args)
         {
+            //list of test birbs
+            var birds = new List<Bird>
+            {
+                new MallardDuck(),
+                new EmperorPenguin(),
+                new BaldEagle(),
+                new Ostrich(),
+                new YellowRubberDuck()
+            };
+            //counts and prints total amount of birds in "birds" list
+            Console.WriteLine($"Number of birds in the simulation: {birds.Count}");
+            foreach (var bird in birds)
+            {
+                //loops until for every bird in the list it types its type and flight ability
+                Console.WriteLine($"Here is the {bird.GetType().Name}'s movement behavior: {bird.PerformFly()}");
+            }
 
         }
     }
